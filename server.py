2cb2b06b0b3b71f9378ac16c599f85dd95a8daea
ken <1610057945@qq.com>
2023-04-06 17:09:54 +0800

    Implement the server

--- server.py
+++ server.py
@@ -1,18 +1,36 @@
-from flask import Flask
-import configuration
-from nju_qr_py import njuQrCode
-from nju_login import nju_login
+from flask import Flask,send_from_directory,redirect
+from . import configuration
+from .nju_qr_py import njuQrCode
+from .nju_login import nju_login
 
 app = Flask("NJU QR server")
 castgc=None
 
 @app.route("/")
-def index():
+def err():
     return '<p>网址没粘完咩～</p>'
 
 @app.route(configuration.path+'/qr')
 def qr():
+    global castgc
+    app.logger.info("Calling qr()...")
     if not castgc:
-        castgc=nju_login.login(configuration.username, configuration.password)
+        app.logger.info("Loggin in...")
+        login_response=nju_login.login(configuration.username, configuration.password)
+        app.logger.info(f"Login cookie: {login_response.cookies}")
+        castgc=nju_login.login(configuration.username, configuration.password).cookies['CASTGC']
 
-    qr_content=njuQrCode.get_qr_content(castgc)
+    app.logger.info("Getting qr content...")
+    qr_content=njuQrCode.getQrContent(castgc)
+
+    app.logger.info("Done getting qr content.")
+    return qr_content
+
+@app.route(configuration.path)
+def index():
+    return redirect(configuration.path+'/index.html')
+
+@app.route(configuration.path+'/<path:path>')
+def static_files(path):
+    return send_from_directory('static', path)
+
