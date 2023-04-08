4d9d561b81d1df036898e8d22d321b60807232f4
ken <xyhken@icloud.com>
2023-04-08 20:23:07 +0800

    Reduce memory footprint
    By reducing workers. Use threads.

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ source "${HOME}/conda/etc/profile.d/conda.sh"
 source "${HOME}/conda/etc/profile.d/mamba.sh"
 conda activate
 
-gunicorn -w 10 -b 0.0.0.0:80 'code:app'
+gunicorn -w 1 --threads 4 -b 0.0.0.0:80 'code:app'
